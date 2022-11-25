9152a2eca76ce7bf0bae41d4e3e61d3901a80011
mindkeeper <fcb.nyak@gmail.com>
2022-11-25 17:37:03 +0700

    add auth reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./auth";
 
-export default combineReducers({});
+export default combineReducers({
+  auth: authReducer,
+});
