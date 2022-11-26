21fc5bafeb1fecd67b58631f25b7335ac54c5884
mindkeeper <fcb.nyak@gmail.com>
2022-11-26 18:27:11 +0700

    add cloudinary env for images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    CLOUDINARY_LINK: process.env.CLOUDINARY_LINK,
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
   },
 };
 
