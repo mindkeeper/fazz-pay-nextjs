b4e7f5b82d3d6c9a9e87715f5ff8bcb66227af84
mindkeeper <fcb.nyak@gmail.com>
2022-11-27 14:58:23 +0700

    history implemented with pagination

---
+++ src/pages/history/index.jsx
@@ -0,0 +1,140 @@
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CardHistory from "src/common/components/CardHistory";
+import Footer from "src/common/components/Footer";
+import Navbar from "src/common/components/Navbar";
+import PageTitle from "src/common/components/PageTitle";
+import Sidebar from "src/common/components/SideBar";
+import styles from "src/common/styles/History.module.css";
+import historyAction from "src/redux/actions/history";
+
+function History() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const auth = useSelector((state) => state.auth);
+  const history = useSelector((state) => state.history);
+  const totalPage = useSelector((state) => state.history.pagination.totalPage);
+  const [query, setQuery] = useState({
+    page: 1,
+    limit: 4,
+    filter: "MONTH",
+  });
+  const [filter, setFilter] = useState(false);
+  const [filterSelect, setfilterSelect] = useState(null);
+  const [dataFound, setDataFound] = useState(false);
+
+  useEffect(() => {
+    router.push(`/history?page=${query.page}filter=${query.filter}`);
+    dispatch(historyAction.getHistoryThunk(auth.userData.token, query));
+    if (history?.data.length > 0) setDataFound(true);
+  }, [query]);
+
+  console.log(totalPage);
+  return (
+    <>
+      <PageTitle title="History" />
+      <Navbar />
+      <div className={styles.container}>
+        <div className={`col-lg-3 ${styles.onMobile}`}>
+          <Sidebar />
+        </div>
+        <aside className={styles["bottom-right"]}>
+          <div className={styles["right-top"]}>
+            <p className={styles["transaction"]}>Transaction History</p>
+            <div className={`${styles.filter} ${styles.filterHead}`}>
+              <div
+                className={styles.show}
+                onClick={() => {
+                  setFilter(filter ? false : true);
+                  console.log(filter);
+                }}
+              >
+                {!filterSelect ? "-- Select Filter --" : filterSelect}
+              </div>
+              {filter && (
+                <div
+                  className={
+                    filter ? styles.filterDownOn : styles.filterDownOff
+                  }
+                >
+                  <p
+                    className={
+                      filter ? styles.filterDownOn2 : styles.filterDownOff
+                    }
+                    onClick={() => {
+                      setQuery({ ...query, filter: "WEEK" });
+                      setfilterSelect("Week");
+                      setFilter(false);
+                      //   router.push("history?filter=WEEK");
+                    }}
+                  >
+                    Week
+                  </p>
+                  <p
+                    className={
+                      filter ? styles.filterDownOn2 : styles.filterDownOff
+                    }
+                    onClick={() => {
+                      setQuery({ ...query, filter: "MONTH" });
+                      setfilterSelect("Month");
+
+                      setFilter(false);
+                      //   router.push("history?filter=MONTH");
+                    }}
+                  >
+                    Month
+                  </p>
+                  <p
+                    className={
+                      filter ? styles.filterDownOn2 : styles.filterDownOff
+                    }
+                    onClick={() => {
+                      setQuery({ ...query, filter: "YEAR" });
+                      setfilterSelect("Year");
+                      setFilter(false);
+                      //   router.push("history?filter=YEAR");
+                    }}
+                  >
+                    Year
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+          {dataFound ? (
+            history.data.map((data, index) => {
+              return <CardHistory data={data} key={index} />;
+            })
+          ) : (
+            <div>
+              <div className={styles["no-data"]}>No Data Available</div>
+            </div>
+          )}
+          <div className={styles["btn-container"]}>
+            <button
+              disabled={query.page === 1 ? true : false}
+              onClick={() => {
+                setQuery({ ...query, page: query.page - 1 });
+              }}
+            >
+              <i className="bi bi-chevron-left"></i>
+            </button>
+            <button
+              disabled={query.page === totalPage ? true : false}
+              onClick={() => {
+                setQuery({ ...query, page: query.page + 1 });
+              }}
+            >
+              <i className="bi bi-chevron-right"></i>
+            </button>
+          </div>
+        </aside>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default History;
