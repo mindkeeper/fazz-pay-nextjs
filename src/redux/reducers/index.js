fd03b7d45e8af009143b80af1ba3d86148c750e6
mindkeeper <fcb.nyak@gmail.com>
2022-11-28 18:59:29 +0700

    combining transfer and dashboard reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth";
+import dashboardReducer from "./dashboard";
 import historyReducer from "./history";
 import topUpReducer from "./topUp";
+import transferReducer from "./transfer";
 import userReducer from "./user";
 
 export default combineReducers({
@@ -9,4 +11,6 @@ export default combineReducers({
   user: userReducer,
   history: historyReducer,
   topUp: topUpReducer,
+  transfer: transferReducer,
+  dashboard: dashboardReducer,
 });
