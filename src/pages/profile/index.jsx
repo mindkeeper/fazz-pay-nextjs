15d9c6a4d89d077dc34c3e9de59e0f579648efe1
mindkeeper <fcb.nyak@gmail.com>
2022-11-29 14:36:07 +0700

    fix import

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Sidebar from "../../components/SideBar";
+import Sidebar from "src/components/SideBar";
 import Footer from "src/components/Footer";
 import css from "src/styles/Profile.module.css";
 import Image from "next/image";
